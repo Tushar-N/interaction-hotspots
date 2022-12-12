9158d7d74a912f533ed0a77973752960e5664d71
Tushar Nagarajan <Tushar-N@users.noreply.github.com>
2022-12-12 12:17:10 -0600

    Update download_data.sh

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -1,9 +1,6 @@
 # Download everything
-# wget --show-progress -O data/hotspots-data.tar.gz https://www.cs.utexas.edu/~tushar/interaction-hotspots/hotspot_data.tar.gz
-# echo "Data downloaded. Extracting files..."
-
-# download data from the link below and save to data/hotspots-data.tar.gz:
-# https://utexas.box.com/s/teku8f66gigeg7qjqzieje95lxnz5r80
+wget --show-progress -O data/hotspots-data.tar.gz https://utexas.box.com/shared/static/j4ets5j30c9gacjkmpve29jd3hsdkcmu.gz
+echo "Data downloaded. Extracting files..."
 
 tar -zxvf data/hotspots-data.tar.gz
 rm -r data/hotspots-data.tar.gz
